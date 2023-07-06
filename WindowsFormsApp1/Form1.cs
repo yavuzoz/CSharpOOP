65292079be5f0658bad2c4bcd05f39ebdacce373
Yavuz Özbay <ozbayavz@gmail.com>
2023-07-06 13:26:24 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.Models;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Author author = new Author();
+            author.AuthorName = "Agatha";
+            author.LastName = "Christie";
+            author.Write("Death on the Nile");
+
+            Author author1 = new Author();
+            author1.AuthorName = "Paulo";
+            author1.LastName = "Coelho";
+            author1.Write("The Alchemist");
+        }
+    }
+}
---
+++ WindowsFormsApp1/Models/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Models
+{
+    internal class Author
+    {
+        public string AuthorName { get; set; }
+        public string LastName { get; set; }
+
+        public void Write(string BookName)
+        {
+            System.Windows.Forms.MessageBox.Show($"{BookName}{AuthorName} {LastName} is the work of a forensic author ");
+
+        }
+    }
+}
